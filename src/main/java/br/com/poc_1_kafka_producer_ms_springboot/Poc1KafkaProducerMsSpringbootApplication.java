391d7d29f4dd2c97352430d839ac2ff477f417b3
leandroltfx <leandroltfbr@hotmail.com>
2026-05-06 20:30:30 -0300

    Renomeia classes e pacotes

---
+++ src/main/java/br/com/poc_1_kafka_producer_ms_springboot/Poc1KafkaProducerMsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.poc_1_kafka_producer_ms_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc1KafkaProducerMsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc1KafkaProducerMsSpringbootApplication.class, args);
+	}
+
+}
